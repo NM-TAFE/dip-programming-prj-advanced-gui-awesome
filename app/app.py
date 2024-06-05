411ad460b85f5277a9eac40197f7dea95d3cdcb6
LUKE DOWNES <DOWNEL@tdm.local>
2024-06-05 17:25:52 +0800

    feat: starting to add audio for video data process

--- app/app.py
+++ app/app.py
@@ -11,6 +11,7 @@ import glob
 import threading
 import pre_process
 from flask_socketio import SocketIO
+from playsound import playsound
 
 
 # Initialise flask app
@@ -21,6 +22,16 @@ filename: Optional[str] = None
 cancel_search_flag: bool = False
 
 socketio = SocketIO(app)
+
+
+def play_sound(audio_file):
+    """
+    Plays a specific sound. Requires the 'playsound' package to be installed.
+    Args: audio_file (string): Path to the audio
+    """
+    if audio_file:
+        playsound(audio_file)
+
 
 @app.context_processor
 def utility_processor():
@@ -159,6 +170,7 @@ def update_video_data():
         return "success"
     elif "capture" in data:
         utils.update_user_video_data(filename, capture=data["capture"])
+        play_sound("../static/audio/capture_success_tone.wav")
         return "success"
     else:
         logging.error("No compatible data type to update")
